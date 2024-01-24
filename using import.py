e3e44c0591357ac4745addb7153f1993ae7f8f14
Caleb Ngeno <82827770+ogc16@users.noreply.github.com>
2024-01-24 13:31:38 +0300

    using import

---
+++ using import.py
@@ -0,0 +1,10 @@
+import random
+while True:
+  x = int(input("Guess a number:"))
+  a=int(input("Enter the lower limit:"))
+  b=int(input("Enter the upper limit:"))
+  if x == random.randint(a,b):
+    print("You guessed right.")
+    break 
+  else:
+    print("You guessed wrong.Try again.")
